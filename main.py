937dd51577fccdce481f83c39cc7b5a6904dcf8e
eremeno <eremenko.oleg26@gmail.com>
2025-04-18 12:06:18 +0200

    oprava

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 """
-projekt_2.py: první projekt do Engeto Online Python Akademie
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
 
 author: Oleg Eremenko
 email: eremenko.oleg26@gmail.com
